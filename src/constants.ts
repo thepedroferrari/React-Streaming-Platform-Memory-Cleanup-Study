57c6313ea6fcd18a9aa175eac0a00a1463940722
thepedroferrari <pedro@pedroferrari.com>
2021-09-08 17:47:19 +0200

    feat: Add constants file and update index.html

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+// Maybe these could be part of Environmental Variables, but to simplify I'm adding them here
+export const API_URL = "https://content.viaplay.se/pc-se/serier/samtliga"
+export const SITE_URL = "http://localhost:3000"
