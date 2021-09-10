e5a55b9a27b9eeb07cb4e29ad5252e275e25ef23
thepedroferrari <pedro@pedroferrari.com>
2021-09-10 13:37:45 +0200

    feat: usePagination hook

---
+++ src/hooks/usePagination.ts
@@ -0,0 +1,36 @@
+import { useRef, useState } from "react"
+
+/**
+ * @returns lastPage: useRef number
+ * @returns next: updates page += 1
+ * @returns page: number
+ * @returns prev: updates page -= 1
+ * @returns updateLastPage: updates the last page available (default -1)
+ * @fires useState
+ * @fires useRef
+ * @emits O(1)
+ */
+export const usePagination = () => {
+  const [page, setPage] = useState(1)
+  const lastPage = useRef(-1)
+
+  const next = () =>
+    setPage((prevState) =>
+      prevState !== lastPage.current && lastPage.current !== -1
+        ? prevState + 1
+        : 1,
+    )
+
+  const prev = () =>
+    setPage((prevState) =>
+      prevState !== 1 && lastPage.current !== -1
+        ? prevState - 1
+        : lastPage.current,
+    )
+
+  const updateLastPage = (pageCount: number) => {
+    lastPage.current = pageCount
+  }
+
+  return { lastPage, next, page, prev, updateLastPage }
+}
