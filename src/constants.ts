d32f577c71a2d1b6cc8e02d435b8ddfa17a97230
thepedroferrari <pedro@pedroferrari.com>
2021-09-12 01:37:50 +0200

    feat: add Empty_type constants to be filled with better defaults

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,12 @@
-import { ViaplayCategoryTitle } from "types/ViaplayApi"
+import {
+  Boxart,
+  Series,
+  Content,
+  Hero,
+  Images,
+  People,
+  ViaplayCategoryTitle,
+} from "types/ViaplayApi"
 
 // Maybe these could be part of Environmental Variables, but to simplify I'm adding them here
 export const API_URL = "https://content.viaplay.se/pc-se/serier"
@@ -32,3 +40,56 @@ export const CATEGORIES: ViaplayCategoryTitle[] = [
   "starzplay",
   "viaplay-originals",
 ]
+
+export const EMPTY_HERO_IMAGE: Hero = {
+  template: "",
+}
+export const EMPTY_BOX_IMAGE: Boxart = {
+  url: "",
+  template: "",
+}
+
+export const EMPTY_SERIES_IMAGES: Images = {
+  boxart: EMPTY_BOX_IMAGE,
+  landscape: EMPTY_BOX_IMAGE,
+  hero169: EMPTY_HERO_IMAGE,
+  coverart169: EMPTY_HERO_IMAGE,
+  coverart23: EMPTY_HERO_IMAGE,
+}
+
+export const EMPTY_SERIES_PEOPLE: People = {
+  actors: [],
+  directors: [],
+  participants: [],
+}
+
+export const EMPTY_SERIES_SERIES: Series = {
+  title: "",
+  synopsis: "",
+  season: {
+    title: "",
+    availability: {
+      svod: {
+        start: "",
+        end: "",
+        planInfo: {
+          isRental: false,
+          isPurchase: false,
+        },
+      },
+    },
+  },
+  seasons: -1,
+  seriesGuid: "",
+}
+
+export const EMPTY_SERIE_CARD: Content = {
+  images: EMPTY_SERIES_IMAGES,
+  people: EMPTY_SERIES_PEOPLE,
+  parentalRating: "",
+  series: EMPTY_SERIES_SERIES,
+  synopsis: "",
+  production: {
+    year: -1,
+  },
+}
