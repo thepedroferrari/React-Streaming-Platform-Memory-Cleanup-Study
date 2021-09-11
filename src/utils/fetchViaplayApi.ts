3233278eafdfb8eaccb14aeaea0ed458abced681
thepedroferrari <pedro@pedroferrari.com>
2021-09-12 01:38:21 +0200

    feat: Function Overloading for the fetch api to be more generic

--- src/utils/fetchViaplayApi.ts
+++ src/utils/fetchViaplayApi.ts
@@ -1,48 +1,74 @@
-import { ViaplayCategoryTitle, ViaplaySeriesPage } from "types/ViaplayApi"
+import {
+  ViaplayBlock,
+  ViaplayCategoryTitle,
+  ViaplaySeriesPage,
+} from "types/ViaplayApi"
 import { API_URL, SITE_URL } from "../constants"
+
+interface FetchViaplayApi {
+  controller: AbortController
+  category?: ViaplayCategoryTitle
+  page?: number
+  url?: string
+}
+
+export async function fetchViaplayApi(
+  settings: Pick<FetchViaplayApi, "controller" | "category" | "page">,
+): Promise<ViaplaySeriesPage>
+
+export async function fetchViaplayApi(
+  settings: Pick<FetchViaplayApi, "controller" | "url">,
+): Promise<ViaplayBlock>
 
 /**
  * @param controller AbortController
  * @param category ViaplayCategoryTitle
  * @param page number
  * @returns Promisified HTTP Response for the ViaplaySeriesPage
- * @description Fetches the Viaplay API Endpoint for series
+ * @description Fetches the Viaplay API Endpoint for series. Created with a
+ * function overloading for the settings to help utilising the function and
+ * making it generic.
  * @fires window.fetch
  * @emits O(1)
  */
-export const fetchViaplayApi = async (
-  controller: AbortController,
-  category: ViaplayCategoryTitle,
-  page = 1,
-): Promise<ViaplaySeriesPage> => {
+export async function fetchViaplayApi({
+  controller,
+  category,
+  page,
+  url,
+}: FetchViaplayApi): Promise<ViaplaySeriesPage | ViaplayBlock> {
   const config: RequestInit = {
     method: "GET",
     signal: controller.signal,
   }
 
+  // Handles the overloading
+  const urlToFetch = url || `${API_URL}/${category}?pageNumber=${page}`
+
   try {
-    return await window
-      .fetch(`${API_URL}/${category}?pageNumber=${page}`, config)
-      .then(async (response) => {
-        if (response.status === 401) {
-          // refresh the page for the user
-          window.location.assign(SITE_URL)
-          return Promise.reject(
-            new Error("Could not reach the server, please try again later."),
-          )
-        }
+    return await window.fetch(urlToFetch, config).then(async (response) => {
+      if (response.status === 401) {
+        // refresh the page for the user
+        window.location.assign(SITE_URL)
+        return Promise.reject(
+          new Error("Could not reach the server, please try again later."),
+        )
+      }
 
-        const data: ViaplaySeriesPage = await response.json()
+      const data: ViaplaySeriesPage | ViaplayBlock = await response.json()
 
-        if (response.ok) return data
-        return Promise.reject(data)
-      })
+      if (response.ok) {
+        return data
+      }
+      return Promise.reject(data)
+    })
   } catch (error) {
+    // Send to DataDog or similar
     if (!controller.signal.aborted) {
-      // Send to DataDog or similar
-      // eslint-disable-next-line no-console
-      console.error({ error })
+      throw new Error("Error fetching the API while not aborted")
     }
+    // eslint-disable-next-line no-console
+    console.error("Error:", error)
     return Promise.reject(error)
   }
 }
